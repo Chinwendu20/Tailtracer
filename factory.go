bfd725ae1354bd64f55ddb81a6c1aec04f11ed60
Maureen <amaka013@gmail.com>
2022-12-09 15:50:12 +0100

    Updated factory.go

--- factory.go
+++ factory.go
@@ -3,13 +3,14 @@ package tailtracer
 import (
 	"context"
 	"strconv"
-	"go.opentelemetry.io/collector/consumer"
+
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer"
 )
 
 const (
-	typeStr = "tailtracer"
+	typeStr         = "tailtracer"
 	defaultInterval = 1
 )
 
@@ -17,25 +18,24 @@ func CreateDefaultConfig() component.ReceiverConfig {
 
 	return &Config{
 		ReceiverSettings: config.NewReceiverSettings(component.NewID(typeStr)),
-		Interval: strconv.Itoa(defaultInterval),
+		Interval:         strconv.Itoa(defaultInterval),
 	}
 }
 
 // func CreateTracesReceiver(_ context.Context, params component.ReceiverCreateSettings, bcfg Config,
-// 	nextConsumer Component.consumer.Traces) (component.TracesReceiver, error){
-// 		return nil, nil
-// 	}
-func createTracesReceiver(s context.Context, p component.ReceiverCreateSettings, baseCfg component.Config, consumer consumer.Traces) (component.TracesReceiver, error) {
-		return nil,nil
-	  }
+//
+//	nextConsumer Component.consumer.Traces) (component.TracesReceiver, error){
+//		return nil, nil
+//	}
+func createTracesReceiver(s context.Context, p component.ReceiverCreateSettings, baseCfg component.ReceiverConfig, consumer consumer.Traces) (component.TracesReceiver, error) {
+	return nil, nil
+}
 
-  
-func NewFactory()component.ReceiverFactory{
+func NewFactory() component.ReceiverFactory {
 	return component.NewReceiverFactory(
 		typeStr,
 		CreateDefaultConfig,
-		component.WithTracesReceiver(createTracesReceiver))
-
+		component.WithTracesReceiver(createTracesReceiver, component.StabilityLevelStable),
 	)
 
 }
